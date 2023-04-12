2d8d1403f493d3a4465f821115b1eb61c498f1a1
Ajay-26 <aj.sailopal@gmail.com>
2023-04-12 17:30:53 -0500

    update

--- src/main.py
+++ src/main.py
@@ -8,9 +8,9 @@ y = np.sin(x)
 df = pd.DataFrame(np.concatenate((x,y),axis=0))
 df.to_csv('test_file.csv')
 
-page_manager = PageManager()
+#page_manager = PageManager()
 ui.markdown("Dataset options:")
-ui.upload(on_upload=lambda e: page_manager.receive_data_upload(e)).classes('max-w-full')
-ui.input(label="Choose the column you want as labels").on('keydown.enter', lambda e: page_manager.choose_label(e.value))
+#ui.upload(on_upload=lambda e: page_manager.receive_data_upload(e)).classes('max-w-full')
+#ui.input(label="Choose the column you want as labels").on('keydown.enter', lambda e: page_manager.choose_label(e.value))
 
 ui.run(port=8080)
